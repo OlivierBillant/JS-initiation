f1d553e6706c6f6b19520f0839b86c9c716a88de
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-09 11:05:29 +0200

    ADD : cours initiation

--- initiation.js
+++ initiation.js
@@ -1,1 +1,26 @@
 console.log("Hello, world ! from JS")
+
+var nombre = 2;
+let nombre2 = 5;
+// Difference entre let et var : https://stackoverflow.com/questions/762011/whats-the-difference-between-using-let-and-var
+// Ajout récent à JS : var variable globale, let : variable locale
+console.log(nombre2);
+nombre2 = "toto";
+console.log(nombre2);
+
+//Operation arithmetiques
+let resultat = nombre + 3;
+console.log(resultat);
+
+//Concatenations
+let prenom = "Adrien";
+console.log("Bonjour : " + prenom);
+
+//Operations logiques
+console.log(1 > 3);
+
+//Operation de comparaison
+let chiffre = 1;
+let chiffreString = "1";
+console.log(chiffre == chiffreString);
+console.log(chiffre === chiffreString);
